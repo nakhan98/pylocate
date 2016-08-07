c767002acf9bca147d6542383d347bc82c23afe1
Nasef Khan <inbox@nakhan.net>
2016-08-07 23:27:07 +0100

    Fixed docstring

--- pylocate.py
+++ pylocate.py
@@ -5,8 +5,8 @@ pylocate
 Quick and dirty python version of mlocate
 
 Options:
-    -u, --update Re-index       SEARCH_DIRS and update pickle store
-    -f, --find <search_term>    Search directories for file using <search_term>
+    -u, --update                Re-index SEARCH_DIRS and update pickle store
+    -f, --find <search_term>    Search SEARCH_DIRS for file using <search_term>
                                 (optional)
 
 Usage:
